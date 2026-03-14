91376bcf42b79b77f9dd0995a39fc421a14c5665
First Last <711daniyal@gmail.com>
2026-03-14 19:56:09 +0500

    added

--- backend/src/app.js
+++ backend/src/app.js
@@ -55,10 +55,10 @@ app.use('/api/v1/orders', orderRoutes);
 
 
 // 1️⃣ Serve static files (images, js, css)
-app.use(express.static(path.join(__dirname, "../public")));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../public/index.html"));
-});
+// app.use(express.static(path.join(__dirname, "../public")));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../public/index.html"));
+// });
 // 2️⃣ React SPA fallback
 // app.get((req, res) => {
 //   res.sendFile(path.resolve(__dirname, "../public/index.html"));
